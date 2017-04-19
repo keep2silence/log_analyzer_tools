3c1dc73c4da50356ecb885d246338f9e45baeabc
duht <dl_dht@aliyun.com>
2017-04-19 20:34:46 +0800

    dev

--- ex01_run.pl
+++ ex01_run.pl
@@ -2,6 +2,8 @@
 
 use strict;
 
+my $PROG="./ex01";
+
 my $prob = 0.4;
 my $count = 1;
 
@@ -10,7 +12,7 @@ for ($prob = 0.4; $prob <= 0.6; $prob = $prob + 0.02) {
 	for (; $count <= 5; ++$count) {
 		`mkdir out_${count}_${prob}`;
 		printf ("./ex01 a.csv $count $prob\n");
-		`./ex01 TestCase.170410.172932.log.exe.csv $count $prob > runlog_${count}_${prob}.log`;
+		`$PROG TestCase.170410.172932.log.exe.csv $count $prob > runlog_${count}_${prob}.log`;
 		`mv 2016*.csv runlog_${count}_${prob}.log out_${count}_${prob}`;
 		`mv out_stat.csv > out_${count}_${prob}/profit.csv`;
 	}
