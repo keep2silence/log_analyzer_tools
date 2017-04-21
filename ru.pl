6c001cb5ee95f4673afb63986bdb576f04e9f997
duht <duht@aliyun.com>
2017-04-21 14:50:13 +0800

    dev

--- ru.pl
+++ ru.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl -w
-
 use strict;
 
-my $UP_SIGNAL_PROB = 4;
-my $DOWN_SIGNAL_PROB = -3;
+if (@ARGV == 0) {
+	printf ("./ru.pl open_valve offset_valve max_posi show_detail signal_file\n");
+	printf ("show_detail: 0 -- 只统计最后的信号个数和总盈亏\n");
+	exit;
+}
 
-my $UP_OFFSET_PROB = 2.5;
-my $DOWN_OFFSET_PROB = -2.5;
+my $UP_SIGNAL_PROB = $ARGV[0];
+my $DOWN_SIGNAL_PROB = 0 - $UP_SIGNAL_PROB;
+my $UP_OFFSET_PROB = $ARGV[1];
+my $DOWN_OFFSET_PROB = 0 - $UP_OFFSET_PROB;
+my $max_posi = $ARGV[2];
+my $show_detail = $ARGV[3];
+
+# printf ("$UP_SIGNAL_PROB, $DOWN_SIGNAL_PROB, $UP_OFFSET_PROB, $DOWN_OFFSET_PROB, $max_posi, $show_detail\n");
+# exit;
 
 my $first_line = 0;
 my $net_posi = 0;
@@ -17,13 +26,17 @@ my $buy_open_price = 0;
 my $sell_open_price = 0;
 my $profit = 0;
 my $sum_profit = 0;
+my $signal_count = 0;
 
-my $max_posi = 1;
+my $signal_file = $ARGV[4];
 
-while (<>) {
+# printf ("|$signal_file|\n");
+open (FILE, "$signal_file") or die open "$signal_file";
+
+while (<FILE>) {
 	if ($first_line == 0) {
 		$first_line = 1;
-		print ($_);
+		#print ($_);
 		next;
 	}
 
@@ -56,20 +69,24 @@ while (<>) {
 	}
 
 	if ((@buy_open_prices < $max_posi) && ($all[15] >= $UP_SIGNAL_PROB)) {
-	#if ($all[15] >= $UP_SIGNAL_PROB) {
 		$line = "$line" . ",1," . "$all[11],$sum_profit";
 		++$net_posi;
+		++$signal_count;
 		$buy_open_price = $all[11];
 		push @buy_open_prices, $buy_open_price;
 	}
 
 	if ((@sell_open_prices < $max_posi) && ($all[15] <= $DOWN_SIGNAL_PROB)) {
-	# if ($all[15] <= $DOWN_SIGNAL_PROB) {
 		$line = "$line" . ",-1," . "$all[9],$sum_profit";
 		--$net_posi;
+		++$signal_count;
 		$sell_open_price = $all[9];
 		push @sell_open_prices, $sell_open_price;
 	}
 	
-	printf ("$line\n");
+	if ($show_detail != 0) {
+		printf ("$line\n");
+	}
 }
+close FILE;
+printf ("$signal_count,$sum_profit\n");
---
+++ ru_run.pl
@@ -0,0 +1,24 @@
+#!/usr/bin/perl -w
+
+use strict;
+
+my $PROG="./ru.pl";
+
+my $open_valve = 4;
+my $offset_valve = 2;
+
+my $signal_file = $ARGV[0];
+my $BaseName;
+if ($ARGV[0] =~ /(\d+)/) {
+	$BaseName = $1;
+}
+mkdir $BaseName;
+
+for (; $open_valve <= 7.02; $open_valve = $open_valve + 0.2) {
+	$offset_valve = 1;
+	for (; $offset_valve <= 3.02; $offset_valve += 0.1) {
+		# printf ("$PROG $open_valve $offset_valve 1 0 $signal_file\n");
+		`$PROG $open_valve $offset_valve 1 0 $signal_file > $BaseName/${signal_file}_${open_valve}_${offset_valve}_1.csv`;
+		# print "$PROG $open_valve $offset_valve 1 0 $signal_file > $BaseName/${signal_file}_${open_valve}_${offset_valve}_1.csv\n";
+	}
+}
